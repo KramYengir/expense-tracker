7dd368a1cd7fd8e544541e6a163dcd5199e03648
KramYengir <kramyengir@gmail.com>
2024-06-26 16:22:27 +0100

    add Header to main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 const roboto = Roboto({ subsets: ["latin"], weight: "400" });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={roboto.className}>{children}</body>
+        <body className={roboto.className}>
+          <Header />
+          <main className="container">{children}</main>
+        </body>
       </html>
     </ClerkProvider>
   );
