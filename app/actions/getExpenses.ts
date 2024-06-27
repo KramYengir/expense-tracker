712abae9bc10e03038b67d005b9329191528315b
KramYengir <kramyengir@gmail.com>
2024-06-27 16:37:19 +0100

    create actions to fetch income & expenses

---
+++ app/actions/getExpenses.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+
+export const getExpenses = async (): Promise<{
+  expenses?: number;
+  error?: string;
+}> => {
+  // get the user from the database
+  const { userId } = auth();
+
+  // if user is not in the database, return an error
+  if (!userId) {
+    return { error: "User not found" };
+  }
+
+  // get all transactions for the user
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+
+  // if there are no transactions, return error
+  if (!transactions || transactions.length === 0) {
+    return { error: "No transactions found" };
+  }
+
+  // get a total of all the positive transactions using reduce
+  const expenses = transactions.reduce((acc, curr) => {
+    if (curr.amount < 0) {
+      return acc - curr.amount;
+    }
+    return acc;
+  }, 0);
+
+  return { expenses };
+};
--- components/IncomeExpense.tsx
+++ components/IncomeExpense.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { getIncome } from "@/app/actions/getIncome";
+import { getExpenses } from "@/app/actions/getExpenses";
+import { addCommas } from "@/lib/utils";
 
-const IncomeExpense = () => {
+const IncomeExpense = async () => {
+  const { income } = await getIncome();
+  const { expenses } = await getExpenses();
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">£3000</p>
+        <p className="money plus">£{addCommas(income ?? 0)}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">£2000</p>
+        <p className="money minus">£{addCommas(expenses ?? 0)}</p>
       </div>
     </div>
   );
