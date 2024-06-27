aa0b2376c80f5164f1189cec5b30357958b4f944
KramYengir <kramyengir@gmail.com>
2024-06-27 15:15:17 +0100

    useref hook for form resetting

--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -3,8 +3,12 @@
 import React from "react";
 import addTransaction from "@/app/actions/addTransaction";
 import { toast } from "react-toastify";
+import { useRef } from "react";
 
 const AddTransaction = () => {
+  // a ref to the form element
+  const formRef = useRef<HTMLFormElement>(null);
+
   const clientAction = async (formData: FormData) => {
     const { data, error } = await addTransaction(formData);
 
@@ -12,13 +16,14 @@ const AddTransaction = () => {
       toast.error(error);
     } else {
       toast.success("Sucessfully added transaction");
+      formRef.current?.reset();
     }
   };
 
   return (
     <>
       <h3>Add Transaction</h3>
-      <form action={clientAction}>
+      <form action={clientAction} ref={formRef}>
         <div className="form-control">
           <label htmlFor="text">Name</label>
           <input
